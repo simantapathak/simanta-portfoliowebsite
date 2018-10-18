7b7dbca10d9ac8fc0b35ec1d8581ed89ea933af4
Sim Path <buy4less2017@gmail.com>
2018-10-17 23:07:58 -0500

    jquery

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
 console.log("Hi..Welcome to my Portfolio site!")
+$(document).ready(function() {
+
+  $(".project").on("mouseenter", function() {
+      $(this).find(".project-details").fadeIn();
+      });
+    $(".project").on("mouseleave", function() {
+      $(this).find(".project-details").fadeOut();
+      });
+  });
